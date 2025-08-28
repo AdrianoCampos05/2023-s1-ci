08d46c9618b14f241242ddd3800b5293a995de79
Adriano Ribeiro Franulovic Campos <a173253@dac.unicamp.br>
2025-08-28 09:03:02 -0300

    Linting

--- simple_web_app/test_unit.py
+++ simple_web_app/test_unit.py
@@ -78,8 +78,8 @@ def test_8_chars_with_no_upper_case_password_should_raise_exception():
 
 
 def test_8_chars_with_invalid_special_character_password_should_raise_exception():
-    #given
-    validator = PasswordValidator(content="aBc123?.")
-    with pytest.raises(IllegalCharacterException): # then
+    # given
+    validator = PasswordValidator(content="aBc123?/")
+    with pytest.raises(IllegalCharacterException):  # then
         # when
         validator.password_validate()
